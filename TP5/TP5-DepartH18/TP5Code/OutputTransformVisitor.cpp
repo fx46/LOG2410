43f3d648b2c3c0ca1a6da6f11f37f0a2e5e7d04b
FX Legault <31199346+fx46@users.noreply.github.com>
2018-04-12 23:08:42 -0400

    Fin de la classe OutputTransformVisitor

--- TP5/TP5-DepartH18/TP5Code/OutputTransformVisitor.cpp
+++ TP5/TP5-DepartH18/TP5Code/OutputTransformVisitor.cpp
@@ -6,6 +6,19 @@ void OutputTransformVisitor::visit(Objet3DPart & obj)
 {
 	// Imprimer tous les triangles contenus dans l'objet
 	// Meme methode que OutputVisitor::visit(Objet3DPart& obj)
+	auto it = obj.triangle_cbegin();
+	for (int index = 0; it != obj.triangle_cend(); it++, index++){
+		m_stream << m_indent 
+				 << "T" 
+				 << index 
+				 << ":" 
+				 << it->s1()
+				 << " | " 
+				 << it->s2() 
+				 << " | " 
+				 << it->s3() 
+				 << std::endl;
+	}
 }
 
 void OutputTransformVisitor::visit(Objet3DComposite & obj)
@@ -16,4 +29,12 @@ void OutputTransformVisitor::visit(Objet3DComposite & obj)
 	//     - Invoquer la methode TransformVisitor::visit(Objet3DComposite &) de 
 	//       la classe de base  pour visiter les enfants
 	//     - Restaurer l'indentation
+	m_indent += "   ";
+	m_stream << m_indent
+			 << "COMPOSITE:"
+			 << std::endl;
+
+	TransformVisitor::visit(obj);
+
+	m_indent = m_indent.substr(0, m_indent.length() - 3);
 }
